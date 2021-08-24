17a45f94c5a4e6792ded36e48d0038b0d60a7fae
psmith <psmith@ricssoftware.com>
2021-08-24 17:13:08 -0400

    added file

---
+++ GitExample/GitExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExample
+{
+    public class GitExample
+    {
+        public GitExample()
+        {
+
+        }
+
+        public GitExample(string exampleProp)
+        {
+            this.ExampleProp = exampleProp;
+        }
+
+        public string ExampleProp { get; set; }
+    }
+}
